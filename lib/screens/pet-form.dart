3046e533fbc5a3d8969344afaf42f0b633ece6e6
Kevin <kevin.clift.b@gmail.com>
2024-09-13 21:10:22 -0300

    layout de flutterflow

--- lib/screens/pet-form.dart
+++ lib/screens/pet-form.dart
@@ -27,54 +27,72 @@ class _PetFormState extends State<PetForm> {
   }
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return SafeArea(
+      child: Column(
+      mainAxisSize: MainAxisSize.max,
       children: [
-        Center(
-          child: Container(
-            margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-            width: 150,
-            height: 150,
-            decoration: const BoxDecoration(shape: BoxShape.circle,
-              color: Colors.grey
+        Expanded(child: Container(
+          width: 394,
+          height: 200,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0, -0.5),
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwzfHxkb2d8ZW58MHx8fHwxNzI1OTEwMjM2fDA&ixlib=rb-4.0.3&q=80&w=1080',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        ),
+        Divider(
+          thickness: 2,
+          color: Color.fromARGB(20, 0, 0, 0),
+        ),
+        Align(
+          alignment: AlignmentDirectional(0.83, -0.13),
+          child: TextFormField(
+            maxLength: 250,
+            maxLines: null,
+            decoration: InputDecoration(
+              isDense: true,
+              filled: true,
+              labelText: "Descripcion",
+              hintText: "Escribe una descripcion",
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Color.fromARGB(20, 0, 0, 0),
+                  width: 2,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
           ),
         ),
-        Form(
-            key: _formKey,
-            child: Column(
-            children: [
-              TextFormField(
-                controller: _ageController,
-                decoration: const InputDecoration(
-                  labelText: "Edad",
-                ),
-                onChanged: (value){
-                  print(value);
-                },
-              ),
-              DropdownButtonFormField(
-                decoration: const InputDecoration(
-                  labelText: "Sexo",
-                ),
-                onChanged: (value){
-                  _genderController = value!;
-                },
-                value: "hembra",
-                items: const <DropdownMenuItem>[
-                  DropdownMenuItem(
-                    child: Text("Hembra"),
-                    value: "hembra",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Macho"),
-                    value: "macho",
-                  ),
-                  ]
-              ),
-              ElevatedButton(onPressed: (){_submitForm(context);},child: const Text("Submit"))
-              ]
-        )),
+        Expanded(child: 
+        Align(alignment: AlignmentDirectional(0, 1),
+
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          ElevatedButton(onPressed: (){}, child: Text("Cancelar")),
+          ElevatedButton(onPressed: (){}, child: Text("Guardar")),
+        ]),
+        )
+        )
       ]
-    );
+    ));
   }
 }
